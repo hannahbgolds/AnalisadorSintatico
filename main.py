397ecad3c530d92d439d8947890cc75025b3b6aa
Hannah <hannahbgoldstein@gmail.com>
2025-07-01 21:19:01 -0300

    mais testes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from parser import parse_input
 
 if __name__ == '__main__':
-    caminho = input("Caminho do arquivo .obs: ").strip()
+    caminho = input("Caminho do arquivo .obsact: ").strip()
     with open(caminho, "r") as f:
         dados = f.read()
         parse_input(dados)
